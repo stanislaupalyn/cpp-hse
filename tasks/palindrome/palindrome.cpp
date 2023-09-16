acfa6fe23879ce36824496727268dbdde198d289
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 22:45:48 +0300

    empty check removed

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,9 +1,6 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    if (str.empty()) {
-        return true;
-    }
     int right_pointer = static_cast<int>(str.size()) - 1;
     for (int left_pointer = 0; left_pointer < right_pointer; ++left_pointer) {
         if (str[left_pointer] == ' ') {
