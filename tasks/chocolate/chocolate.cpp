58bccdf687303b6bc387c1b9b4a9dbe1d8277541
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 20:58:31 +0300

    chocolate solved

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,5 +1,17 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
+    // check vertical slices
+    for (int length = 1; length <= height; ++length) {
+        if (length * width == slices_amount) {
+            return true;
+        }
+    }
+    // check horizontal slices
+    for (int length = 1; length <= width; ++length) {
+        if (length * height == slices_amount) {
+            return true;
+        }
+    }
     return true;
 };
