614a49da45b37697b862debdd899dcc9d436a72a
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 20:04:02 +0300

    replaced numbers with constants

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,8 @@
 #include "is_leap.h"
 
+const int ONE_HUNDRED = 100;
+const int FOUR_HUNDRED = 400;
+
 bool IsLeap(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0;
 }
