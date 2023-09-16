9a570fd50428b2dd0b6d73017ebb00e3a3dc99a3
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 20:47:32 +0300

    min_divisor solved

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,5 +1,10 @@
 #include "min_divisor.h"
 
 int64_t MinDivisor(int64_t number) {
-    return 2;
+    for (int64_t i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return i;
+        }
+    }
+    return number;
 }
