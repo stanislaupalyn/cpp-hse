2e43d72414d4a30614cbb264df12719d392e6626
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 21:57:05 +0300

    has been rewritten without endless loop

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -3,20 +3,17 @@
 bool IsPalindrome(const std::string& str) {
     size_t left_pointer = 0;
     size_t right_pointer = str.size() - 1;
-    while (left_pointer < right_pointer) {
+    for (size_t left_pointer = 0; left_pointer < right_pointer; ++left_pointer) {
         if (str[left_pointer] == ' ') {
-            ++left_pointer;
             continue;
         }
-        if (str[right_pointer] == ' ') {
+        while (right_pointer > left_pointer && str[right_pointer] == ' ') {
             --right_pointer;
-            continue;
         }
         if (str[left_pointer] != str[right_pointer]) {
             return false;
         }
-        ++left_pointer;
-        --right_pointer;
+        right_pointer--;
     }
     return true;
 }
