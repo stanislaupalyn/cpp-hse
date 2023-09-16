4258df252bf59789208c1586e179fc389da290ad
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 21:36:22 +0300

    palindrome solved

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,24 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    size_t leftPointer = 0;
+    size_t rightPointer = str.size() - 1;
+    
+    while (leftPointer < rightPointer) {
+        if (str[leftPointer] == ' ') {
+            ++leftPointer;
+            continue;
+        }
+        if (str[rightPointer] == ' ') {
+            --rightPointer;
+            continue;
+        }
+        if (str[leftPointer] != str[rightPointer]) {
+            return false;
+        }
+        ++leftPointer;
+        --rightPointer;
+    }
+
+    return true;
 }
