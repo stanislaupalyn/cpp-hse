aab618f8bcf5b67f3f4d6b5a264da3964c9d2dee
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 23:24:48 +0300

    wtf

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,10 +1,6 @@
 #include "reduce_fraction.h"
 
-#include <numeric>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    int64_t common_divisor = std::gcd(numerator, denominator);
-    int64_t new_numerator = numerator / common_divisor;
-    int64_t new_denominator = denominator / common_divisor;
-    return new_numerator + new_denominator;
+    return 1;
 }
