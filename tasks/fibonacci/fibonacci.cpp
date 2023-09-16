97d5f0d6e80171a19a791075035830657f3a644a
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 20:10:49 +0300

    fibonacci solved

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,12 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    int64_t a = 0;
+    int64_t b = 1;
+    for (int i = 0; i < n; i++) {
+        int64_t t = a + b;
+        a = b;
+        b = t;
+    }
+    return a;
 }
