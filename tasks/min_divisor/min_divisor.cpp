62bfacf205460a78e5ca9c330a8466c292a99de7
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 21:04:47 +0300

    naming changed and switched to prefix increment

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,9 +1,9 @@
 #include "min_divisor.h"
 
 int64_t MinDivisor(int64_t number) {
-    for (int64_t i = 2; i * i <= number; i++) {
-        if (number % i == 0) {
-            return i;
+    for (int64_t divisor = 2; divisor * divisor <= number; ++divisor) {
+        if (number % divisor == 0) {
+            return divisor;
         }
     }
     return number;
