0729428c3b4c5ee2cc66b031339ebd861992944a
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-16 21:59:23 +0300

    unused variable deleted

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,9 +1,8 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    size_t left_pointer = 0;
     size_t right_pointer = str.size() - 1;
-    for (size_t left_pointer = 0; left_pointer < right_pointer; ++left_pointer) {
+    for (size_t left_pointer = 0; left_pointer < right_pointer; ++left_pointer, right_pointer--) {
         if (str[left_pointer] == ' ') {
             continue;
         }
@@ -13,7 +12,6 @@ bool IsPalindrome(const std::string& str) {
         if (str[left_pointer] != str[right_pointer]) {
             return false;
         }
-        right_pointer--;
     }
     return true;
 }
