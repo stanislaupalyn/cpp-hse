6cb461a6cdced968e7bd376ada6d68fdfc1036fa
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 23:29:33 +0300

    formated

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -3,9 +3,9 @@
 #include <cctype>
 #include <cstdint>
 
-constexpr char MIN_CODE = 33;   // NOLINT
-constexpr char MAX_CODE = 126;  // NOLINT
-constexpr size_t MIN_LENGTH = 8;  // NOLINT
+constexpr char MIN_CODE = 33;      // NOLINT
+constexpr char MAX_CODE = 126;     // NOLINT
+constexpr size_t MIN_LENGTH = 8;   // NOLINT
 constexpr size_t MAX_LENGTH = 14;  // NOLINT
 
 bool ValidatePassword(const std::string& password) {
