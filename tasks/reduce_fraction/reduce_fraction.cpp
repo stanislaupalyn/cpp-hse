74dd384c84ddff0ca07275ea7918e135837c76a7
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 21:21:06 +0300

    extra variables removed

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -4,7 +4,5 @@
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
     int64_t common_divisor = std::gcd(numerator, denominator);
-    int64_t new_numerator = numerator / common_divisor;
-    int64_t new_denominator = denominator / common_divisor;
-    return new_numerator + new_denominator;
+    return numerator / common_divisor + denominator / common_divisor;
 }
