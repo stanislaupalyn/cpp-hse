8ff9199a0b3c0d27481d6c6397d407439bc2a4a0
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 22:36:55 +0300

    two space

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 
-constexpr int32_t ONE_HUNDRED = 100; // NOLINT
-constexpr int32_t FOUR_HUNDRED = 400; // NOLINT
+constexpr int32_t ONE_HUNDRED = 100;  // NOLINT
+constexpr int32_t FOUR_HUNDRED = 400;  // NOLINT
 
 bool IsLeap(int32_t year) {
     return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0;
