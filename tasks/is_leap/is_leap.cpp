f925e46d6dc8f9448938f9915e899e18b00f58e2
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 22:11:56 +0300

    test

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -6,5 +6,5 @@ constexpr int32_t ONE_HUNDRED = 100; // NO LINT
 constexpr int32_t FOUR_HUNDRED = 400; // NO LINT
 
 bool IsLeap(int32_t year) {
-    return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0;
+    return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0; // NO LINT
 }
