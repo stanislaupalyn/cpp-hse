8a07119cab5e1e25d15f18086c6ab6048a6f83c0
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 22:36:23 +0300

    remove extra nolint

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -6,5 +6,5 @@ constexpr int32_t ONE_HUNDRED = 100; // NOLINT
 constexpr int32_t FOUR_HUNDRED = 400; // NOLINT
 
 bool IsLeap(int32_t year) {
-    return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0; // NOLINT
+    return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0;
 }
