d59de9c0878d71ee24a0c165b546643ca5c3cd22
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 22:09:49 +0300

    fix no lint

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -2,10 +2,8 @@
 
 #include <cstdint>
 
-// NO LINT
-constexpr int32_t ONE_HUNDRED = 100;
-// NO LINT
-constexpr int32_t FOUR_HUNDRED = 400;
+constexpr int32_t ONE_HUNDRED = 100; // NO LINT
+constexpr int32_t FOUR_HUNDRED = 400; // NO LINT
 
 bool IsLeap(int32_t year) {
     return (year % 4 == 0 && year % ONE_HUNDRED != 0) || year % FOUR_HUNDRED == 0;
