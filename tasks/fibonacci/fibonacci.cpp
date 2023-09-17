132801f3f5f097d93ff810f051d0f2565360f915
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-17 21:01:15 +0300

    fixed width types and naming

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,12 +1,12 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    int64_t a = 0;
-    int64_t b = 1;
-    for (int i = 0; i < n; i++) {
-        int64_t t = a + b;
-        a = b;
-        b = t;
+    int64_t previous = 0;
+    int64_t current = 1;
+    for (int32_t i = 0; i < n; ++i) {
+        int64_t next = previous + current;
+        previous = current;
+        current = next;
     }
     return a;
 }
