30949ff7c4feb50ed9e2f7d6ea47957566b5e48b
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-27 11:48:03 +0300

    sort_students solved

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,4 +1,47 @@
 #include "sort_students.h"
 
+#include <algorithm>
+
+// Date comparison
+bool operator<(const Date& lhs, const Date& rhs) {
+    if (lhs.year != rhs.year) {
+        return lhs.year < rhs.year;
+    }
+    if (lhs.month != rhs.month) {
+        return lhs.month < rhs.month;
+    }
+    return lhs.day < rhs.day;
+}
+
+bool operator==(const Date& lhs, const Date& rhs) {
+    return lhs.year == rhs.year && lhs.month == rhs.month && lhs.day == rhs.day;
+}
+
+bool operator!=(const Date& lhs, const Date& rhs) {
+    return !(lhs == rhs);
+}
+
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
+    if (sortKind == SortKind::Name) {
+        sort(students.begin(), students.end(), [](const Student& lhs, const Student& rhs) {
+            if (lhs.last_name != rhs.last_name) {
+                return lhs.last_name < rhs.last_name;
+            }
+            if (lhs.name != rhs.name) {
+                return lhs.name < rhs.name;
+            }
+            return lhs.birth_date < rhs.birth_date;
+        });
+    }
+    if (sortKind == SortKind::Date) {
+        sort(students.begin(), students.end(), [](const Student& lhs, const Student& rhs) {
+            if (lhs.birth_date != rhs.birth_date) {
+                return lhs.birth_date < rhs.birth_date;
+            }
+            if (lhs.last_name != rhs.last_name) {
+                return lhs.last_name < rhs.last_name;
+            }
+            return lhs.name < rhs.name;
+        });
+    }
 }
