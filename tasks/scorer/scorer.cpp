301f15193aa9546df93eb7a4efa8a86b238b97f4
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-28 19:52:05 +0300

    assert added

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,7 @@
 #include "scorer.h"
 
+#include <cassert>
+
 struct ProblemStatus {
     bool is_last_success = false;
     bool is_merge_request_closed = true;
@@ -23,19 +25,19 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
         switch (current_event.event_type) {
             case EventType::CheckSuccess:
                 status_of[{current_event.student_name, current_event.task_name}].is_last_success = true;
-                status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed = true;
                 break;
 
             case EventType::CheckFailed:
                 status_of[{current_event.student_name, current_event.task_name}].is_last_success = false;
-                status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed = true;
                 break;
 
             case EventType::MergeRequestClosed:
+                assert(!(status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed));
                 status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed = true;
                 break;
 
             case EventType::MergeRequestOpen:
+                assert((status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed));
                 status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed = false;
                 break;
         }
