560fac7c743793c717387d8acb3d7713ac66818b
Stanislau Palyn <stanislavpolyn@yandex.ru>
2023-09-28 20:00:46 +0300

    fix

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -33,13 +33,13 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
 
             case EventType::MergeRequestClosed:
                 // assert(!(status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed));
-                status_of[{current_event.student_name, current_event.task_name}].opened_merge_requests++;
+                status_of[{current_event.student_name, current_event.task_name}].opened_merge_requests--;
+                assert((status_of[{current_event.student_name, current_event.task_name}].opened_merge_requests) >= 0);
                 break;
 
             case EventType::MergeRequestOpen:
                 // assert((status_of[{current_event.student_name, current_event.task_name}].is_merge_request_closed));
-                status_of[{current_event.student_name, current_event.task_name}].opened_merge_requests--;
-                assert((status_of[{current_event.student_name, current_event.task_name}].opened_merge_requests) >= 0);
+                status_of[{current_event.student_name, current_event.task_name}].opened_merge_requests++;
                 break;
         }
     }
